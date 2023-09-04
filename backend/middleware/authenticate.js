0b2ee4fc81a40b3f97245aebbf203161b4a61112
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-09-04 08:47:35 +0530

    Prepared authentication middleware

---
+++ backend/middleware/authenticate.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const User = require('../models/User.model.js');
+
+const authenticate = async (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token missing' });
+  }
+
+  try {
+    const tokenWithoutBearer = token.replace('Bearer ', ''); // Remove "Bearer " prefix
+    const decoded = await jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET_KEY);
+    const user = await User.findById(decoded.id); 
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid user' });
+    }
+
+    req.user = {
+      id: user.id,
+      userType: user.userType,
+    };
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authenticate;
